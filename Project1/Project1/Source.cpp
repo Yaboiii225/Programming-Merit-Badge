3282e8562f9cbf8565faae944fa43e61f2326fe1
Yaboiii225 <Yaboiii225@gmail.com>
2018-06-28 13:56:06 -0400

    Added files to repository

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,7 @@
+#include <string>
+#include <random>
+using namespace std;
+ void selection_sort(vector<int&)
+	 int  mindex = 0
+	 for (int 1 = 0; 100)
+		 cout << list[11st]
---
+++ src/net/Michael/Main.java
@@ -0,0 +1,14 @@
+package net.Michael;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a word to see if its a palidrome");
+        int I = scanner.nextInt();
+        System.out.println("That is a palidrome");
+
+        System.out.println("so cool");
+    }
