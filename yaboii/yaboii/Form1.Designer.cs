313814332ffa76ee4c73e3c6417a385605c25f8d
Yaboiii225 <Yaboiii225@gmail.com>
2018-06-29 14:03:14 -0400

    message

---
+++ yaboii/yaboii/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace yaboii
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webClient1 = new System.Net.WebClient();
+            this.SuspendLayout();
+            // 
+            // webClient1
+            // 
+            this.webClient1.AllowReadStreamBuffering = false;
+            this.webClient1.AllowWriteStreamBuffering = false;
+            this.webClient1.BaseAddress = "";
+            this.webClient1.CachePolicy = null;
+            this.webClient1.Credentials = null;
+            this.webClient1.Encoding = ((System.Text.Encoding)(resources.GetObject("webClient1.Encoding")));
+            this.webClient1.Headers = ((System.Net.WebHeaderCollection)(resources.GetObject("webClient1.Headers")));
+            this.webClient1.QueryString = ((System.Collections.Specialized.NameValueCollection)(resources.GetObject("webClient1.QueryString")));
+            this.webClient1.UseDefaultCredentials = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 338);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Net.WebClient webClient1;
+    }
+}
+
